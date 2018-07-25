37e40e619d36d0c698986b5202bea48754548c3c
胡海娟 <332350793@qq.com>
2018-07-25 21:10:48 +0800

    配置完vue环境以及项目的开端

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import index from './components/index.vue'
+import vipcenter from './components/vipcenter'
+import orderlist from './components/sons/orderlist.vue'
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: [{
+      path: '/',
+      component: index
+    },
+
+
+    {
+      path: '/index',
+      component: index
+    },
+    {
+      path: '/vipcenter',
+      component: vipcenter
+    },
+    {
+      path: '/orderlist',
+      component: orderlist
+    },
+  ]
+})
+// import './assets/statics/site/css/style.css'
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App)
-}).$mount('#app')
+})
